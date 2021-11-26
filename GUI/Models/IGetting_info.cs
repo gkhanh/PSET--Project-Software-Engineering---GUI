075c01239d68e388595dd515a0ea726cd27dac26
Gia Khanh Le <493341@student.saxion.nl>
2021-11-26 12:56:25 +0100

    added get_data class and getting_info interface

---
+++ GUI/Models/IGetting_info.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GUI.Models
+{
+    public interface IGetting_info
+    {
+        string Name { get; set; }
+        string Location { get; set; }
+        int Humidity { get; set; }
+        int temperature { get; set; }
+        int air_pressure { get; set; }
+        int light_info { get; set; }
+        string day { get; set; }
+
+    }
+
+    
+}

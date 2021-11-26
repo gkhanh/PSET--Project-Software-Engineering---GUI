bc8338d743635c57530632ccfe690079345293e3
Larissa Berkelder <larissaaa_@outlook.com>
2021-11-26 11:15:12 +0100

    Added somethings to the aboutpage

--- GUI/Views/WeeklyTemperature.xaml.cs
+++ GUI/Views/WeeklyTemperature.xaml.cs
@@ -3,18 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microcharts;
+using SkiaSharp;
+
+
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
+
 
 namespace GUI.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WeeklyTemperature : ContentPage
     {
+        private readonly ChartEntry[] entries = new[]
+        {
+            new ChartEntry(10)
+            {
+                Label = "Monday",
+                ValueLabel = "10",
+                Color = SKColor.Parse("#3FBCCA")
+            },
+            new ChartEntry(12)
+            {
+                Label = "Tuesday",
+                ValueLabel = "12",
+                Color = SKColor.Parse("#3FBCCA")
+            },
+            new ChartEntry(10)
+            {
+                Label = "Wednesday",
+                ValueLabel = "10",
+                Color = SKColor.Parse("#3FBCCA")
+            },
+            new ChartEntry(14)
+            {
+                Label = "Thursday",
+                ValueLabel = "14",
+                Color = SKColor.Parse("#3FBCCA")
+            },
+             new ChartEntry(9)
+            {
+                Label = "Friday",
+                ValueLabel = "9",
+                Color = SKColor.Parse("#3FBCCA")
+            },
+              new ChartEntry(12)
+            {
+                Label = "Saturday",
+                ValueLabel = "12",
+                Color = SKColor.Parse("#3FBCCA")
+            },
+              new ChartEntry(8)
+            {
+                Label = "Sunday",
+                ValueLabel = "8",
+                Color = SKColor.Parse("#3FBCCA")
+            }
+        };
         public WeeklyTemperature()
         {
             InitializeComponent();
+            TemperatureWeek.Chart = new LineChart {  Entries = entries, LineMode = LineMode.Straight, BackgroundColor = SKColors.Transparent };
+            //chartViewLine.Chart = new LineChart { Entries = entries, LineMode = LineMode.Straight };
+
         }
     }
 }

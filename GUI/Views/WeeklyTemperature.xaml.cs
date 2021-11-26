1fd67eb126f17d1622a31ead4fcd134680ad795b
Larissa Berkelder <larissaaa_@outlook.com>
2021-11-26 11:34:50 +0100

    Temperatureweek

--- GUI/Views/WeeklyTemperature.xaml.cs
+++ GUI/Views/WeeklyTemperature.xaml.cs
@@ -24,49 +24,56 @@ namespace GUI.Views
             {
                 Label = "Monday",
                 ValueLabel = "10",
-                Color = SKColor.Parse("#3FBCCA")
+                Color = SKColor.Parse("#3FBCCA"),
+                ValueLabelColor = SKColor.Parse("#FFFFFF")
             },
             new ChartEntry(12)
             {
                 Label = "Tuesday",
                 ValueLabel = "12",
-                Color = SKColor.Parse("#3FBCCA")
+                Color = SKColor.Parse("#3FBCCA"),
+                ValueLabelColor = SKColor.Parse("#FFFFFF")
             },
             new ChartEntry(10)
             {
                 Label = "Wednesday",
                 ValueLabel = "10",
-                Color = SKColor.Parse("#3FBCCA")
+                Color = SKColor.Parse("#3FBCCA"),
+                ValueLabelColor = SKColor.Parse("#FFFFFF")
             },
             new ChartEntry(14)
             {
                 Label = "Thursday",
                 ValueLabel = "14",
-                Color = SKColor.Parse("#3FBCCA")
+                Color = SKColor.Parse("#3FBCCA"),
+                ValueLabelColor = SKColor.Parse("#FFFFFF")
             },
              new ChartEntry(9)
             {
                 Label = "Friday",
                 ValueLabel = "9",
-                Color = SKColor.Parse("#3FBCCA")
+                Color = SKColor.Parse("#3FBCCA"),
+                ValueLabelColor = SKColor.Parse("#FFFFFF")
             },
               new ChartEntry(12)
             {
                 Label = "Saturday",
                 ValueLabel = "12",
-                Color = SKColor.Parse("#3FBCCA")
+                Color = SKColor.Parse("#3FBCCA"),
+                ValueLabelColor = SKColor.Parse("#FFFFFF")
             },
               new ChartEntry(8)
             {
                 Label = "Sunday",
                 ValueLabel = "8",
-                Color = SKColor.Parse("#3FBCCA")
+                Color = SKColor.Parse("#3FBCCA"),
+                ValueLabelColor = SKColor.Parse("#FFFFFF")
             }
         };
         public WeeklyTemperature()
         {
             InitializeComponent();
-            TemperatureWeek.Chart = new LineChart {  Entries = entries, LineMode = LineMode.Straight, BackgroundColor = SKColors.Transparent };
+            TemperatureWeek.Chart = new LineChart {  Entries = entries, LineMode = LineMode.Straight, BackgroundColor = SKColors.Transparent, LabelColor = SKColor.Parse("#FFFFFF") };
             //chartViewLine.Chart = new LineChart { Entries = entries, LineMode = LineMode.Straight };
 
         }
